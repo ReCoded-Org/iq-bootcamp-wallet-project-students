97f7528da09aa758c072400f067e2c0a28ee5ea8
Shirin Muataz <shirinmuataz@gmail.com>
2020-06-18 21:01:20 +0300

    finished no-wallet-view

---
+++ script.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('#create-wallet');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    /*let newWindow = open('/', 'example', 'width=300,height=300')
+    newWindow.focus();*/
+    let win = open('create-wallet-form.html', "", 'width=300,height=300');
+    win.focus();
+
+
+})
+
+
+
+class Wallet {
+
+}
+class Transaction {
+
+}
+class Expense {
+
+}
+class Income {
+
+}
+class Currency {
+
+}
+
+
+
+
