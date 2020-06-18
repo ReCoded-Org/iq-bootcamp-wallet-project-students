1a2a03ab6d554efdab3501cb5bd22188428a4b10
Shirin Muataz <shirinmuataz@gmail.com>
2020-06-18 23:36:58 +0300

    Showed the Modal with Bootstrap4

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-const form = document.querySelector('#create-wallet');
+/*const form = document.querySelector('#create-wallet');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    /*let newWindow = open('/', 'example', 'width=300,height=300')
-    newWindow.focus();*/
-    let win = open('create-wallet-form.html', "", 'width=300,height=300');
-    win.focus();
+    /*let params = `scrollbars=1,resizable=1,width=1000,height=580,left=0,top=0`;
+    let neWin = open('create-wallet-form.html', "", params);
+    neWin.focus();
+
+    neWin.onload = function () {
+        let html = `<div style="font-size:30px">Welcome!</div>`;
+        neWin.document.body.insertAdjacentHTML('afterbegin', html);*/
+//};
+
+//})
+
+/*$('#openModal').on('click', function () {
+    $('#button1').trigger('focus')
+})*/
+
+/*let x = document.querySelector('#exampleModal');
+x.on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+})*/
 
 
-})
+
+
 
 
 
