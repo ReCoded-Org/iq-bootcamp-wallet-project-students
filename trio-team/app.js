fa8a669ec7af7356e2cf04ab5fd103a1386cd842
Hussien-Talal-Majeed <hossientalal2@gmail.com>
2020-06-19 12:27:28 +0300

    adding Wallet class

--- trio-team/app.js
+++ trio-team/app.js
@@ -2,12 +2,34 @@
 1- create a class called wallet has the properties 
     name, startingBalance, currency, description, array of transactions(which each element is an object contains amount, type, date, tags and notes),
     and an unique id for each instance,
-    and a method that calculates the total balance by iterating over the transactions of the insatnce.
-2- an array to store all the instances of the class
-3- a void function to store the array in the local storage
-4- a void function to retrieve data from the local storage when the page is reloded
+    and a method that calculates the total balance by iterating over the transactions of the insatnce.✅
+2- an array to store all the instances of the class. ✅
+3- a void function to store the array in the local storage. ✅
+4- a void function to retrieve data from the local storage when the page is reloded. ✅
 5- eventListeners for the buttons
-6- a void function to insert the transation to the DOM and modify the instance
+6- a void function to insert the transaction to the DOM and modify the instance
 7- a void function to add the names of the wallets to the dropdown in the navbar
 8- a void function for creating a new wallet and pushing it to the array of wallets
 */
+
+class Wallet{
+    constructor(name, startingBalance, currency, description){
+        this.name = name;
+        this.startingBalance = startingBalance;
+        this.currency = currency;
+        this.description = description;
+        this.id = Math.floor(Math.random()*1000000);
+    }
+    transactions = [];
+    get totalBalance(){
+        let total = 0;
+        transactions.forEach(transaction => {
+            if(transaction.type === 'income')
+                total += transaction.amount;
+            else
+                total -= transaction.amount;
+        })
+        return this.startingBalance + total;
+    }
+}
+
