c5b5616948e7f88ce7f3dd80bd2439e555ccb2f1
Hussien-Talal-Majeed <hossientalal2@gmail.com>
2020-06-19 10:12:03 +0300

    adding instructions

---
+++ trio-team/app.js
@@ -0,0 +1,13 @@
+/*
+1- create a class called wallet has the properties 
+    name, startingBalance, currency, description, array of transactions(which each element is an object contains amount, type, date, tags and notes),
+    and an unique id for each instance,
+    and a method that calculates the total balance by iterating over the transactions of the insatnce.
+2- an array to store all the instances of the class
+3- a void function to store the array in the local storage
+4- a void function to retrieve data from the local storage when the page is reloded
+5- eventListeners for the buttons
+6- a void function to insert the transation to the DOM and modify the instance
+7- a void function to add the names of the wallets to the dropdown in the navbar
+8- a void function for creating a new wallet and pushing it to the array of wallets
+*/
