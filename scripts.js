d0c525eae1d841b348b9d24b8d874a1928cdacad
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-04-28 20:56:32 +0300

    Co-authored-by: Payam0057 <pamoo@users.noreply.github.com>

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const createWallet = document.getElementById("createWalletBtn");
+const createBtn = document.getElementById("create-btn");
+const nameInp = document.getElementById("name-inp");
+const currency1 = document.getElementById("currency-inp-1");
+const currency2 = document.getElementById("currency-inp-2");
+const balanceInp = document.getElementById("balance-inp");
+const description = document.getElementById("description-inp");
+
+class Wallet {
+  constructor(name, currency, balance, description) {
+    this.name = name;
+    this.currency = currency;
+    this.balance = balance;
+    this.description = description;
+  }
+}
+class Transaction {}
